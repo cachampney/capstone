8c84ba6cf76805c97ee4e76460012b86052faf2e
Depforce17 <51565138+Depforce17@users.noreply.github.com>
2023-10-08 12:07:27 -0500

    Updated names, layout and other necessary items to bring it into alignment with Design Diagram.

--- ui_component.py
+++ ui_component.py
@@ -1,186 +1,170 @@
 # ui_component.py
 import os, traceback
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QListWidget, QListWidgetItem, QMessageBox, QFileDialog
-from data_manager import DataManager
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, \
+    QListWidget, QListWidgetItem, QMessageBox, QFileDialog, QComboBox
 
-class InitiativeTrackerApp(QWidget):
+
+class BudgetTrackerApp(QWidget):
     def __init__(self):
         super().__init__()
 
-        # Initialize the data management component
-        self.data_manager = DataManager()
-
-        # Initialize the turn index to 0 (first participant's turn)
-        self.turn_index = 0
-
         # UI Widgets
-        self.participant_name_input = QLineEdit()
-        self.initiative_input = QLineEdit()
-        self.initiative_list = QListWidget() 
-        self.current_turn_label = QLabel("Current Turn:")
-        self.on_deck_label = QLabel("On Deck:")
-        self.third_in_line_label = QLabel("Third in Line:")
+        self.transaction_type = QComboBox()
+        self.date = QLineEdit()
+        self.vendor = QLineEdit()
+        self.transaction_amount = QLineEdit()
+        self.transaction_note = QLineEdit()
+        self.transaction_category = QComboBox()
+        self.transaction_list = QListWidget()
+        self.total_expenses_label = QLabel("Total Expenses:")
+        self.total_income_label = QLabel("Total Income:")
+        self.balance_label = QLabel("Balance:")
+
+        # Adding pre-determined strings for Transaction Type and Category drop down boxes
+        self.transaction_type.addItem("Expense")
+        self.transaction_type.addItem("Income")
+
+        self.transaction_category.addItem("Utilities")
+        self.transaction_category.addItem("Gas")
+        self.transaction_category.addItem("Entertainment")
+        self.transaction_category.addItem("Rent/Housing")
+        self.transaction_category.addItem("Groceries")
+        self.transaction_category.addItem("Other")
 
         # Buttons
-        self.add_participant_button = QPushButton("Add Participant")
-        self.remove_participant_button = QPushButton("Remove Participant")
-        self.save_encounter_button = QPushButton("Save Encounter")
-        self.load_encounter_button = QPushButton("Load Encounter")
-        self.delete_encounter_button = QPushButton("Delete Encounter")
-        self.next_turn_button = QPushButton("Next Turn")
-        self.previous_turn_button = QPushButton("Previous Turn")
+        self.add_transaction_button = QPushButton("Add Transaction")
+        self.remove_transaction_button = QPushButton("Remove Transaction")
+        self.save_transaction_button = QPushButton("Save Budget")
+        self.load_transaction_button = QPushButton("Load Budget")
+        self.delete_transaction_button = QPushButton("Delete Budget")
 
         # Connect buttons to their respective functions
-        self.add_participant_button.clicked.connect(self.add_participant)
-        self.remove_participant_button.clicked.connect(self.remove_participant)
-        self.save_encounter_button.clicked.connect(self.save_encounter_dialog)
-        self.load_encounter_button.clicked.connect(self.load_encounter_dialog)
-        self.delete_encounter_button.clicked.connect(self.delete_encounter_dialog)
-        self.next_turn_button.clicked.connect(self.next_turn)
-        self.previous_turn_button.clicked.connect(self.previous_turn)
+        self.add_transaction_button.clicked.connect(self.add_transaction)
+        self.remove_transaction_button.clicked.connect(self.remove_transaction)
+        self.save_transaction_button.clicked.connect(self.save_budget_dialog)
+        self.load_transaction_button.clicked.connect(self.load_budget_dialog)
+        self.delete_transaction_button.clicked.connect(self.delete_budget_dialog)
 
         # Set up the layout
         layout = QVBoxLayout()
 
-        # Add participant name and initiative on the same row
-        participant_initiative_layout = QHBoxLayout()
-        participant_initiative_layout.addWidget(QLabel("Participant Name:"))
-        participant_initiative_layout.addWidget(self.participant_name_input)
-        participant_initiative_layout.addWidget(QLabel("Initiative:"))
-        participant_initiative_layout.addWidget(self.initiative_input)
-        layout.addLayout(participant_initiative_layout)
+        # Add Transaction Type and date on the first row
+        budget_first_row_layout = QHBoxLayout()
+        budget_first_row_layout.addWidget(QLabel("Transaction Type:"))
+        budget_first_row_layout.addWidget(self.transaction_type)
+        budget_first_row_layout.addWidget(QLabel("Date:"))
+        budget_first_row_layout.addWidget(self.date)
+        layout.addLayout(budget_first_row_layout)
 
-        # Add participant buttons on the same row
-        participant_buttons_layout = QHBoxLayout()
-        participant_buttons_layout.addWidget(self.add_participant_button)
-        participant_buttons_layout.addWidget(self.remove_participant_button)
-        layout.addLayout(participant_buttons_layout)
+        # Add Vendor and Transaction Amount to the second row
+        budget_second_row_layout = QHBoxLayout()
+        budget_second_row_layout.addWidget(QLabel("Vendor:"))
+        budget_second_row_layout.addWidget(self.vendor)
+        budget_second_row_layout.addWidget(QLabel("Transaction Amount:"))
+        budget_second_row_layout.addWidget(self.transaction_amount)
+        layout.addLayout(budget_second_row_layout)
 
-        # Add encounter buttons
-        layout.addWidget(self.save_encounter_button)
-        layout.addWidget(self.load_encounter_button)
-        layout.addWidget(self.delete_encounter_button)
-        
-        # Add display to show list of participants
-        layout.addWidget(self.initiative_list)
-        
+        # Add Transaction Note and Category drop down box onto the third row
+        budget_third_row_layout = QHBoxLayout()
+        budget_third_row_layout.addWidget(QLabel("Note:"))
+        budget_third_row_layout.addWidget(self.transaction_note)
+        budget_third_row_layout.addWidget(QLabel("Category:"))
+        budget_third_row_layout.addWidget(self.transaction_category)
+        layout.addLayout(budget_third_row_layout)
+
+        # Add "add/remove transaction buttons" onto the fourth row
+        budget_fourth_row_layout = QHBoxLayout()
+        budget_fourth_row_layout.addWidget(self.add_transaction_button)
+        budget_fourth_row_layout.addWidget(self.remove_transaction_button)
+        layout.addLayout(budget_fourth_row_layout)
+
+        # Add buttons to save, delete, and load a budget
+        layout.addWidget(self.save_transaction_button)
+        layout.addWidget(self.load_transaction_button)
+        layout.addWidget(self.delete_transaction_button)
+
+        # Add display to show list of transactions
+        layout.addWidget(self.transaction_list)
+
         # Add the labels to the layout
-        layout.addWidget(self.current_turn_label)
-        layout.addWidget(self.on_deck_label)
-        layout.addWidget(self.third_in_line_label)
-        
-        # Add the buttons to the layout
-        layout.addWidget(self.next_turn_button)
-        layout.addWidget(self.previous_turn_button)
+        layout.addWidget(self.total_expenses_label)
+        layout.addWidget(self.total_income_label)
+        layout.addWidget(self.balance_label)
 
         self.setLayout(layout)
-        self.setWindowTitle("Initiative Tracker")
+        self.setWindowTitle("Budget Tracker")
 
         # Update the turn labels based on the initial initiative order
-        self.update_turn_labels()
-        
+        # self.update_turn_labels()
+
         self.show()
-        
-    def next_turn(self):
-        initiative_order = self.data_manager.get_initiative_order()
-        if len(initiative_order) > 1:
-            # Increment the turn index, and handle returning to the top of the initiative order
-            self.turn_index = (self.turn_index + 1) % len(initiative_order)
-            self.update_turn_labels()
 
-    def previous_turn(self):
-        initiative_order = self.data_manager.get_initiative_order()
-        if len(initiative_order) > 1:
-            # Decrement the turn index, and handle cycling back to the bottom of the initiative order
-            self.turn_index = (self.turn_index - 1) % len(initiative_order)
-            self.update_turn_labels()
-        
-    def update_turn_labels(self):
-        initiative_order = self.data_manager.get_initiative_order()
-
-        if initiative_order:
-            # Use the turn index to determine the current turn participant
-            current_turn, current_initiative = initiative_order[self.turn_index]
-            self.current_turn_label.setText(f"Current Turn: {current_turn} ({current_initiative})")
-
-            # Use the turn index to determine the on deck participant (next turn)
-            on_deck_index = (self.turn_index + 1) % len(initiative_order)
-            on_deck_turn, on_deck_initiative = initiative_order[on_deck_index]
-            self.on_deck_label.setText(f"On Deck: {on_deck_turn} ({on_deck_initiative})")
-
-            # Use the turn index to determine the third in line participant
-            third_in_line_index = (self.turn_index + 2) % len(initiative_order)
-            third_in_line_turn, third_in_line_initiative = initiative_order[third_in_line_index]
-            self.third_in_line_label.setText(f"Third in Line: {third_in_line_turn} ({third_in_line_initiative})")
-
-
-    def add_participant(self):
-        name = self.participant_name_input.text()
-        initiative = self.initiative_input.text()
+    def add_transaction(self):
+        name = self.transaction_type.text()
+        initiative = self.date.text()
         if name and initiative.isdigit():
             initiative = int(initiative)
-            self.data_manager.add_participant(name, initiative)
-            self.update_initiative_list()
+            self.data_manager.add_transaction(name, initiative)
+            self.update_transaction_list()
 
             # Clear the input fields
-            self.participant_name_input.clear()
-            self.initiative_input.clear()
+            self.transaction_type.clear()
+            self.date.clear()
             self.update_turn_labels()
         else:
-            QMessageBox.warning(self, "Invalid Input", "Please enter a valid participant name and a positive integer initiative.")
+            QMessageBox.warning(self, "Invalid Input",
+                                "Please enter a valid Transaction Type and a positive integer initiative.")
 
-    def remove_participant(self):
-        selected_items = self.initiative_list.selectedItems()
+    def remove_transaction(self):
+        selected_items = self.transaction_list.selectedItems()
         if selected_items:
             for item in selected_items:
                 text = item.text()
                 participant, _ = text.split(": ")
-                self.data_manager.remove_participant(participant)
-            self.update_initiative_list()
+                self.data_manager.remove_transaction(participant)
+            self.update_transaction_list()
             self.update_turn_labels()
 
-    def save_encounter_dialog(self):
+    def save_budget_dialog(self):
         file_path, _ = QFileDialog.getSaveFileName(self, "Save Encounter", "", "Text Files (*.txt)")
         if file_path:
             encounter_name = os.path.splitext(os.path.basename(file_path))[0]
             self.data_manager.save_encounter(encounter_name)
-            self.update_initiative_list()
-        
+            self.update_transaction_list()
 
-    
-
-    def load_encounter_dialog(self):
+    def load_budget_dialog(self):
         file_path, _ = QFileDialog.getOpenFileName(self, "Load Encounter", "", "Text Files (*.txt)")
         if file_path:
-            encounter_name = os.path.splitext(os.path.basename(file_path))[0]                
+            encounter_name = os.path.splitext(os.path.basename(file_path))[0]
             self.data_manager.load_encounter(encounter_name)
-            self.update_initiative_list()       
+            self.update_transaction_list()
 
-    def delete_encounter_dialog(self):
-        
+    def delete_budget_dialog(self):
+
         file_path, _ = QFileDialog.getOpenFileName(self, "Delete Encounter", "", "Text Files (*.txt)")
         if file_path:
-            encounter_name = os.path.splitext(os.path.basename(file_path))[0]                
+            encounter_name = os.path.splitext(os.path.basename(file_path))[0]
             self.data_manager.delete_encounter(encounter_name)
-            self.update_initiative_list()
+            self.update_transaction_list()
 
     def delete_encounter(self):
         encounter_name = self.encounter_name_input.text()
         if encounter_name.isalnum():
             self.data_manager.delete_encounter(encounter_name)
-            self.update_initiative_list()
+            self.update_transaction_list()
 
             # Clear the encounter name input field
             self.encounter_name_input.clear()
 
-    def update_initiative_list(self):
-        self.initiative_list.clear()
+    def update_transaction_list(self):
+        self.transaction_list.clear()
         initiative_order = self.data_manager.get_initiative_order()
         for participant, initiative in initiative_order:
             IOrder = QListWidgetItem(f"{participant}: {initiative}")
-            self.initiative_list.addItem(IOrder)
+            self.transaction_list.addItem(IOrder)
+
 
 if __name__ == "__main__":
     app = QApplication([])
-    window = InitiativeTrackerApp()
+    window = BudgetTrackerApp()
     app.exec_()
