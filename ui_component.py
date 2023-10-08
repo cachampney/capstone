f0817de1411103f0f4632645e2c5109cd1414e9b
Depforce17 <51565138+Depforce17@users.noreply.github.com>
2023-10-08 16:10:19 -0500

    Updated input validation for both dates and transaction amount. Will not allow for character input except a decimal point. Dates must be in the correct format DD/MM/YY, and floats will be shaved to two decimal points.

--- ui_component.py
+++ ui_component.py
@@ -1,103 +1,110 @@
 # ui_component.py
-import os, traceback
+import os, re, traceback
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, \
     QListWidget, QListWidgetItem, QMessageBox, QFileDialog, QComboBox
 
 
 class BudgetTrackerApp(QWidget):
     def __init__(self):
-        super().__init__()
+        try:
+            super().__init__()
 
-        # UI Widgets
-        self.transaction_type = QComboBox()
-        self.date = QLineEdit()
-        self.vendor = QLineEdit()
-        self.transaction_amount = QLineEdit()
-        self.transaction_note = QLineEdit()
-        self.transaction_category = QComboBox()
-        self.transaction_list = QListWidget()
-        self.total_expenses_label = QLabel("Total Expenses:")
-        self.total_income_label = QLabel("Total Income:")
-        self.balance_label = QLabel("Balance:")
+            # UI Widgets
+            self.transaction_type = QComboBox()
+            self.date = QLineEdit()
+            self.vendor = QLineEdit()
+            self.transaction_amount = QLineEdit()
+            self.transaction_note = QLineEdit()
+            self.transaction_category = QComboBox()
+            self.transaction_list = QListWidget()
+            self.total_expenses_label = QLabel("Total Expenses:")
+            self.total_income_label = QLabel("Total Income:")
+            self.balance_label = QLabel("Balance:")
 
-        # Adding pre-determined strings for Transaction Type and Category drop down boxes
-        self.transaction_type.addItem("Expense")
-        self.transaction_type.addItem("Income")
+            # Adding pre-determined strings for Transaction Type and Category drop down boxes
+            self.transaction_type.addItem("Expense")
+            self.transaction_type.addItem("Income")
 
-        self.transaction_category.addItem("Utilities")
-        self.transaction_category.addItem("Gas")
-        self.transaction_category.addItem("Entertainment")
-        self.transaction_category.addItem("Rent/Housing")
-        self.transaction_category.addItem("Groceries")
-        self.transaction_category.addItem("Other")
+            self.transaction_category.addItem("Utilities")
+            self.transaction_category.addItem("Gas")
+            self.transaction_category.addItem("Entertainment")
+            self.transaction_category.addItem("Rent/Housing")
+            self.transaction_category.addItem("Groceries")
+            self.transaction_category.addItem("Other")
 
-        # Buttons
-        self.add_transaction_button = QPushButton("Add Transaction")
-        self.remove_transaction_button = QPushButton("Remove Transaction")
-        self.save_transaction_button = QPushButton("Save Budget")
-        self.load_transaction_button = QPushButton("Load Budget")
-        self.delete_transaction_button = QPushButton("Delete Budget")
+            # Buttons
+            self.add_transaction_button = QPushButton("Add Transaction")
+            self.remove_transaction_button = QPushButton("Remove Transaction")
+            self.save_transaction_button = QPushButton("Save Budget")
+            self.load_transaction_button = QPushButton("Load Budget")
+            self.delete_transaction_button = QPushButton("Delete Budget")
 
-        # Connect buttons to their respective functions
-        self.add_transaction_button.clicked.connect(self.add_transaction)
-        self.remove_transaction_button.clicked.connect(self.remove_transaction)
-        self.save_transaction_button.clicked.connect(self.save_budget_dialog)
-        self.load_transaction_button.clicked.connect(self.load_budget_dialog)
-        self.delete_transaction_button.clicked.connect(self.delete_budget_dialog)
+            # Connect buttons to their respective functions
+            self.add_transaction_button.clicked.connect(self.add_transaction)
+            self.remove_transaction_button.clicked.connect(self.remove_transaction)
+            self.save_transaction_button.clicked.connect(self.save_budget_dialog)
+            self.load_transaction_button.clicked.connect(self.load_budget_dialog)
+            self.delete_transaction_button.clicked.connect(self.delete_budget_dialog)
 
-        # Set up the layout
-        layout = QVBoxLayout()
+            # Verify both transaction amount and the date when text is done being edited
+            self.date.editingFinished.connect(self.validate_date)
+            self.transaction_amount.editingFinished.connect(self.validate_transaction_amount)
 
-        # Add Transaction Type and date on the first row
-        budget_first_row_layout = QHBoxLayout()
-        budget_first_row_layout.addWidget(QLabel("Transaction Type:"))
-        budget_first_row_layout.addWidget(self.transaction_type)
-        budget_first_row_layout.addWidget(QLabel("Date:"))
-        budget_first_row_layout.addWidget(self.date)
-        layout.addLayout(budget_first_row_layout)
+            # Set up the layout
+            layout = QVBoxLayout()
 
-        # Add Vendor and Transaction Amount to the second row
-        budget_second_row_layout = QHBoxLayout()
-        budget_second_row_layout.addWidget(QLabel("Vendor:"))
-        budget_second_row_layout.addWidget(self.vendor)
-        budget_second_row_layout.addWidget(QLabel("Transaction Amount:"))
-        budget_second_row_layout.addWidget(self.transaction_amount)
-        layout.addLayout(budget_second_row_layout)
+            # Add Transaction Type and date on the first row
+            budget_first_row_layout = QHBoxLayout()
+            budget_first_row_layout.addWidget(QLabel("Transaction Type:"))
+            budget_first_row_layout.addWidget(self.transaction_type)
+            budget_first_row_layout.addWidget(QLabel("Date:"))
+            budget_first_row_layout.addWidget(self.date)
+            layout.addLayout(budget_first_row_layout)
 
-        # Add Transaction Note and Category drop down box onto the third row
-        budget_third_row_layout = QHBoxLayout()
-        budget_third_row_layout.addWidget(QLabel("Note:"))
-        budget_third_row_layout.addWidget(self.transaction_note)
-        budget_third_row_layout.addWidget(QLabel("Category:"))
-        budget_third_row_layout.addWidget(self.transaction_category)
-        layout.addLayout(budget_third_row_layout)
+            # Add Vendor and Transaction Amount to the second row
+            budget_second_row_layout = QHBoxLayout()
+            budget_second_row_layout.addWidget(QLabel("Vendor:"))
+            budget_second_row_layout.addWidget(self.vendor)
+            budget_second_row_layout.addWidget(QLabel("Transaction Amount:"))
+            budget_second_row_layout.addWidget(self.transaction_amount)
+            layout.addLayout(budget_second_row_layout)
 
-        # Add "add/remove transaction buttons" onto the fourth row
-        budget_fourth_row_layout = QHBoxLayout()
-        budget_fourth_row_layout.addWidget(self.add_transaction_button)
-        budget_fourth_row_layout.addWidget(self.remove_transaction_button)
-        layout.addLayout(budget_fourth_row_layout)
+            # Add Transaction Note and Category drop down box onto the third row
+            budget_third_row_layout = QHBoxLayout()
+            budget_third_row_layout.addWidget(QLabel("Note:"))
+            budget_third_row_layout.addWidget(self.transaction_note)
+            budget_third_row_layout.addWidget(QLabel("Category:"))
+            budget_third_row_layout.addWidget(self.transaction_category)
+            layout.addLayout(budget_third_row_layout)
 
-        # Add buttons to save, delete, and load a budget
-        layout.addWidget(self.save_transaction_button)
-        layout.addWidget(self.load_transaction_button)
-        layout.addWidget(self.delete_transaction_button)
+            # Add "add/remove transaction buttons" onto the fourth row
+            budget_fourth_row_layout = QHBoxLayout()
+            budget_fourth_row_layout.addWidget(self.add_transaction_button)
+            budget_fourth_row_layout.addWidget(self.remove_transaction_button)
+            layout.addLayout(budget_fourth_row_layout)
 
-        # Add display to show list of transactions
-        layout.addWidget(self.transaction_list)
+            # Add buttons to save, delete, and load a budget
+            layout.addWidget(self.save_transaction_button)
+            layout.addWidget(self.load_transaction_button)
+            layout.addWidget(self.delete_transaction_button)
 
-        # Add the labels to the layout
-        layout.addWidget(self.total_expenses_label)
-        layout.addWidget(self.total_income_label)
-        layout.addWidget(self.balance_label)
+            # Add display to show list of transactions
+            layout.addWidget(self.transaction_list)
 
-        self.setLayout(layout)
-        self.setWindowTitle("Budget Tracker")
+            # Add the labels to the layout
+            layout.addWidget(self.total_expenses_label)
+            layout.addWidget(self.total_income_label)
+            layout.addWidget(self.balance_label)
 
-        # Update the turn labels based on the initial initiative order
-        # self.update_turn_labels()
+            self.setLayout(layout)
+            self.setWindowTitle("Budget Tracker")
 
-        self.show()
+            # Update the turn labels based on the initial initiative order
+            # self.update_turn_labels()
+
+            self.show()
+        except Exception as e:
+            print(f"Exception in validate_date: {str(e)}")
 
     def add_transaction(self):
         name = self.transaction_type.text()
@@ -163,8 +170,37 @@ class BudgetTrackerApp(QWidget):
             IOrder = QListWidgetItem(f"{participant}: {initiative}")
             self.transaction_list.addItem(IOrder)
 
+    def validate_date(self):
+        date_text = self.date.text()
+
+        date_pattern = re.compile(r'\d{2}\/\d{2}/\d{2}')
+
+        if not re.match(date_pattern, date_text):
+            # Show what format it should look like
+            QMessageBox.warning(self, "Invalid Date", "Please enter a date in the format DD/MM/YY")
+
+            # Clear the QLineEdit
+            self.date.clear()
+            self.date.setFocus()
+
+    def validate_transaction_amount(self):
+        amount = self.transaction_amount.text()
+
+        try:
+            check_for_float_type = float(amount)
+            check_for_float_type = "{:.2f}".format(check_for_float_type)
+            self.transaction_amount.setText(check_for_float_type)
+        except:
+            QMessageBox.warning(self, "Invalid Amount", "Must be a float, with no characters except a decimal point")
+            # Clear the QLineEdit
+            self.transaction_amount.clear()
+            self.transaction_amount.setFocus()
+
+
+
 
 if __name__ == "__main__":
+
     app = QApplication([])
     window = BudgetTrackerApp()
     app.exec_()
