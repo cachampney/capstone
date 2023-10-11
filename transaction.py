a0734d8c425ff74199a15631a10b9f15483f27b6
Unorganiz <christiandavis660@gmail.com>
2023-10-10 19:08:36 -0500

    commiting the transaction

---
+++ transaction.py
@@ -0,0 +1,44 @@
+"""
+Module for Transaction Class
+"""
+
+class Transaction:
+    '''
+    transaction_type : str
+    date : str
+    amount : float
+    vendor : str
+    category : str
+    note : str
+    '''
+    # methods
+   
+    #---------------------------------------------------------------------
+    #Forces all arguments to be fullfilled
+    #Or does it just need set to self without the arguments put in place
+    #Need to pull the information from the GUI text boxes initially
+    #---------------------------------------------------------------------
+    
+    def __init__(self, transaction_type : str, date : str, amount : float, vendor : str, category : str, note : str):
+        self.transaction_type = transaction_type
+        self.date = date
+        if not isinstance(amount, float):
+            raise ValueError("Amount in not a float")
+        self.amount = amount
+        self.vendor = vendor
+        self.category = category
+        self.note = note    
+
+    def edit(self, transaction_type, date, amount, vendor, category, note):
+    #--------------------------------------------------------------------------------------    
+    #Go through an text box ties to the variables labled
+    #EX. self.amount = amountBox.text (this is how it's done on C# visual studio gui)
+    #^^ (done initially)
+    #
+    #--------------------------------------------------------------------------------------  
+        self.transaction_type = transaction_type
+        self.date = date
+        self.amount = amount
+        self.vendor = vendor
+        self.category = category
+        self.note = note
