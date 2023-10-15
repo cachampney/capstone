8620cd1a9ca40ba4f4018015c8f30fc9b458dd16
Depforce17 <51565138+Depforce17@users.noreply.github.com>
2023-10-15 11:23:13 -0500

    Lots of updates. 1. enforcing transaction amount works as expected 2. Clear UI when new transaction is added, set focus to transaction type. 3. enforce needing a transaction amount and date prior to adding a transaction 4. added ability to remove transaction 5. Updates total expense and income at the bottom of the window.

--- ui_component.py
+++ ui_component.py
@@ -1,12 +1,21 @@
 # ui_component.py
-import os, re, traceback
+import os, re
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, \
-    QListWidget, QListWidgetItem, QMessageBox, QFileDialog, QComboBox
+    QListWidget, QListWidgetItem, QMessageBox, QFileDialog, QComboBox, QTableWidget, QTableWidgetItem
+from transaction import Transaction
+from budget import Budget
 
 
 class BudgetTrackerApp(QWidget):
+
+    # Import global row, transaction, and budget varible. This will make manipulating these in the methods much easier
+    #to work with.
+    row = 0
+    transaction = Transaction()
+    budget = Budget()
+
     def __init__(self):
-        try:
+
             super().__init__()
 
             # UI Widgets
@@ -15,11 +24,16 @@ class BudgetTrackerApp(QWidget):
             self.vendor = QLineEdit()
             self.transaction_amount = QLineEdit()
             self.transaction_note = QLineEdit()
+            self.transaction_note.setMaxLength(150)
             self.transaction_category = QComboBox()
-            self.transaction_list = QListWidget()
-            self.total_expenses_label = QLabel("Total Expenses:")
-            self.total_income_label = QLabel("Total Income:")
+            self.transaction_table = QTableWidget()
+            self.total_expenses_label = QLabel(f"Total Expenses: {self.budget.total_expenses}")
+            self.total_income_label = QLabel(f"Total Income: {self.budget.total_income}")
             self.balance_label = QLabel("Balance:")
+
+            # Set the number of columns to match the expected input and name them.
+            self.transaction_table.setColumnCount(6)
+            self.transaction_table.setHorizontalHeaderLabels(["Date", "Type", "Amount", "Vendor", "Category", "Note"])
 
             # Adding pre-determined strings for Transaction Type and Category drop down boxes
             self.transaction_type.addItem("Expense")
@@ -32,7 +46,7 @@ class BudgetTrackerApp(QWidget):
             self.transaction_category.addItem("Groceries")
             self.transaction_category.addItem("Other")
 
-            # Buttons
+            # Create buttons
             self.add_transaction_button = QPushButton("Add Transaction")
             self.remove_transaction_button = QPushButton("Remove Transaction")
             self.save_transaction_button = QPushButton("Save Budget")
@@ -46,7 +60,7 @@ class BudgetTrackerApp(QWidget):
             self.load_transaction_button.clicked.connect(self.load_budget_dialog)
             self.delete_transaction_button.clicked.connect(self.delete_budget_dialog)
 
-            # Verify both transaction amount and the date when text is done being edited
+            # Verify transaction amount, date format, and character length on note
             self.date.editingFinished.connect(self.validate_date)
             self.transaction_amount.editingFinished.connect(self.validate_transaction_amount)
 
@@ -89,7 +103,7 @@ class BudgetTrackerApp(QWidget):
             layout.addWidget(self.delete_transaction_button)
 
             # Add display to show list of transactions
-            layout.addWidget(self.transaction_list)
+            layout.addWidget(self.transaction_table)
 
             # Add the labels to the layout
             layout.addWidget(self.total_expenses_label)
@@ -99,52 +113,78 @@ class BudgetTrackerApp(QWidget):
             self.setLayout(layout)
             self.setWindowTitle("Budget Tracker")
 
-            # Update the turn labels based on the initial initiative order
-            # self.update_turn_labels()
-
             self.show()
-        except Exception as e:
-            print(f"Exception in validate_date: {str(e)}")
 
     def add_transaction(self):
-        name = self.transaction_type.text()
-        initiative = self.date.text()
-        if name and initiative.isdigit():
-            initiative = int(initiative)
-            self.data_manager.add_transaction(name, initiative)
-            self.update_transaction_list()
 
-            # Clear the input fields
-            self.transaction_type.clear()
-            self.date.clear()
-            self.update_turn_labels()
-        else:
-            QMessageBox.warning(self, "Invalid Input",
-                                "Please enter a valid Transaction Type and a positive integer initiative.")
+        # Enforce both transaction and date to not be empty before adding the transaction.
+        if self.transaction_amount.text() == "":
+            QMessageBox.warning(self, "Blank transaction amount", "Transaction amount must not be empty.")
+            self.transaction_amount.setFocus()
+            return
+
+        if self.date.text() == "":
+            QMessageBox.warning(self, "Blank date", "Transaction date must not be blank.")
+            self.date.setFocus()
+            return
+
+        # Update transaction object then add it to the table.
+        self.transaction.edit(self.transaction_type.currentText(), self.date.text(), float(self.transaction_amount.text()),
+                              self.vendor.text(), self.transaction_category.currentText(), self.transaction_note.text())
+
+        self.transaction_table.insertRow(self.row)
+        self.transaction_table.setItem(self.row, 0, QTableWidgetItem(self.transaction.date))
+        self.transaction_table.setItem(self.row, 1, QTableWidgetItem(self.transaction.transaction_type))
+        self.transaction_table.setItem(self.row, 2, QTableWidgetItem(str(self.transaction.amount)))
+        self.transaction_table.setItem(self.row, 3, QTableWidgetItem(self.transaction.vendor))
+        self.transaction_table.setItem(self.row, 4, QTableWidgetItem(self.transaction.category))
+        self.transaction_table.setItem(self.row, 5, QTableWidgetItem(self.transaction.note))
+        self.row += 1
+        self.transaction_table.update()
+
+        # Update budget class. This will allow us to save and load budgets easier.
+        self.budget.add_transaction(self.transaction)
+        # Clear fields for end user
+        self.clear_ui()
+        # Update total expenses, income and balance
+        self.update_under_table_hud()
 
     def remove_transaction(self):
-        selected_items = self.transaction_list.selectedItems()
+        selected_items = self.transaction_table.selectedItems()
+
+        # updating the transaction variable to match what is in the row. Easiest way to delete them out of the budget
+        #module
         if selected_items:
-            for item in selected_items:
-                text = item.text()
-                participant, _ = text.split(": ")
-                self.data_manager.remove_transaction(participant)
-            self.update_transaction_list()
-            self.update_turn_labels()
+            selected_row = selected_items[0].row()
+
+            self.transaction_table.setItem(selected_row, 0, QTableWidgetItem(self.transaction.date))
+            self.transaction_table.setItem(selected_row, 1, QTableWidgetItem(self.transaction.transaction_type))
+            self.transaction_table.setItem(selected_row, 2, QTableWidgetItem(str(self.transaction.amount)))
+            self.transaction_table.setItem(selected_row, 3, QTableWidgetItem(self.transaction.vendor))
+            self.transaction_table.setItem(selected_row, 4, QTableWidgetItem(self.transaction.category))
+            self.transaction_table.setItem(selected_row, 5, QTableWidgetItem(self.transaction.note))
+
+            self.transaction_table.removeRow(selected_row)
+            self.budget.delete_transaction(self.transaction)
+        else:
+            QMessageBox.warning(self, "Remove Transaction", "No row was selected.")
+        self.row -= 1
+
+        self.update_under_table_hud()
 
     def save_budget_dialog(self):
         file_path, _ = QFileDialog.getSaveFileName(self, "Save Encounter", "", "Text Files (*.txt)")
         if file_path:
             encounter_name = os.path.splitext(os.path.basename(file_path))[0]
             self.data_manager.save_encounter(encounter_name)
-            self.update_transaction_list()
+            self.update_transaction_table()
 
     def load_budget_dialog(self):
         file_path, _ = QFileDialog.getOpenFileName(self, "Load Encounter", "", "Text Files (*.txt)")
         if file_path:
             encounter_name = os.path.splitext(os.path.basename(file_path))[0]
             self.data_manager.load_encounter(encounter_name)
-            self.update_transaction_list()
+            self.update_transaction_table()
 
     def delete_budget_dialog(self):
 
@@ -152,23 +192,14 @@ class BudgetTrackerApp(QWidget):
         if file_path:
             encounter_name = os.path.splitext(os.path.basename(file_path))[0]
             self.data_manager.delete_encounter(encounter_name)
-            self.update_transaction_list()
+            self.update_transaction_table()
 
-    def delete_encounter(self):
-        encounter_name = self.encounter_name_input.text()
-        if encounter_name.isalnum():
-            self.data_manager.delete_encounter(encounter_name)
-            self.update_transaction_list()
-
-            # Clear the encounter name input field
-            self.encounter_name_input.clear()
-
-    def update_transaction_list(self):
-        self.transaction_list.clear()
-        initiative_order = self.data_manager.get_initiative_order()
-        for participant, initiative in initiative_order:
-            IOrder = QListWidgetItem(f"{participant}: {initiative}")
-            self.transaction_list.addItem(IOrder)
+    def update_transaction_table(self):
+        self.transaction_table.clear()
+        transaction_table = self.data_manager.get_initiative_order()
+        for participant, initiative in transaction_table:
+            tList = QListWidgetItem(f"{participant}: {initiative}")
+            self.transaction_table.addItem(tList)
 
     def validate_date(self):
         date_text = self.date.text()
@@ -190,13 +221,23 @@ class BudgetTrackerApp(QWidget):
             check_for_float_type = float(amount)
             check_for_float_type = "{:.2f}".format(check_for_float_type)
             self.transaction_amount.setText(check_for_float_type)
+
         except:
             QMessageBox.warning(self, "Invalid Amount", "Must be a float, with no characters except a decimal point")
             # Clear the QLineEdit
             self.transaction_amount.clear()
             self.transaction_amount.setFocus()
 
+    def update_under_table_hud(self):
+        self.total_expenses_label.setText(f"Total Expenses: {self.budget.total_expenses}")
+        self.total_income_label.setText(f"Total Income: {self.budget.total_income}")
 
+    def clear_ui(self):
+        self.transaction_amount.clear()
+        self.date.clear()
+        self.transaction_note.clear()
+        self.vendor.clear()
+        self.transaction_type.setFocus()
 
 
 if __name__ == "__main__":
